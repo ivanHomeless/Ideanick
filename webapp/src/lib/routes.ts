86ae368137e5680eee10dee5ea950f8865ffb84c
Dmitry <double0fork@gmail.com>
2025-01-10 11:11:03 +0300

    Add Idea

--- webapp/src/lib/routes.ts
+++ webapp/src/lib/routes.ts
@@ -4,6 +4,7 @@ const getRouteParams = <T extends Record<string, boolean>>(object: T) => {
   
   export const getAllIdeasRoute = () => '/'
   
+  export const getNewIdeaRoute = () => '/ideas/new'
   export const viewIdeaRouteParams = getRouteParams({ ideaNick: true })
   export type ViewIdeaRouteParams = typeof viewIdeaRouteParams
   export const getViewIdeaRoute = ({ ideaNick }: ViewIdeaRouteParams) => `/ideas/${ideaNick}`
