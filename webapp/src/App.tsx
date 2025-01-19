6946b09094e3b680710388e327e1f730f0ce8d91
Dmitry <double0fork@gmail.com>
2025-01-19 08:16:56 +0300

    Added Sign Up Page

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,26 +1,26 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import { Layout } from "./components/Layout"
-import * as routes from "./lib/routes"
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Layout } from './components/Layout'
+import * as routes from './lib/routes'
 import { TrpcProvider } from './lib/trpc'
 import { AllIdeasPage } from './pages/AllIdeasPage'
 import { NewIdeaPage } from './pages/NewIdeaPage'
+import { SignUpPage } from './pages/SignUpPage'
 import { ViewIdeaPage } from './pages/ViewIdeaPage'
 import './styles/global.scss'
-
-
 
 export const App = () => {
   return (
     <TrpcProvider>
       <BrowserRouter>
         <Routes>
-          <Route element={<Layout/>}>
-            <Route path={routes.getAllIdeasRoute()} element={<AllIdeasPage/>}></Route>
+          <Route element={<Layout />}>
+            <Route path={routes.getSignUpRoute()} element={<SignUpPage />} />
+            <Route path={routes.getAllIdeasRoute()} element={<AllIdeasPage />}></Route>
             <Route path={routes.getNewIdeaRoute()} element={<NewIdeaPage />} />
-            <Route path={routes.getViewIdeaRoute(routes.viewIdeaRouteParams)} element={<ViewIdeaPage/>}></Route>            
+            <Route path={routes.getViewIdeaRoute(routes.viewIdeaRouteParams)} element={<ViewIdeaPage />}></Route>
           </Route>
         </Routes>
-      </BrowserRouter>      
+      </BrowserRouter>
     </TrpcProvider>
   )
 }
--- webapp/src/components/Layout/index.tsx
+++ webapp/src/components/Layout/index.tsx
@@ -1,26 +1,31 @@
 import { Link, Outlet } from 'react-router-dom'
-import { getAllIdeasRoute, getNewIdeaRoute } from '../../lib/routes'
+import { getAllIdeasRoute, getNewIdeaRoute, getSignUpRoute } from '../../lib/routes'
 import css from './index.module.scss'
 
 export const Layout = () => {
   return (
     <div className={css.layout}>
-    <div className={css.navigation}>
-      <div className={css.logo}>IdeaNick</div>
-      <ul className={css.menu}>
-        <li className={css.item}>
-          <Link className={css.link} to={getAllIdeasRoute()}>
-            All Ideas
-          </Link>
-        </li>
-        <li className={css.item}>
+      <div className={css.navigation}>
+        <div className={css.logo}>IdeaNick</div>
+        <ul className={css.menu}>
+          <li className={css.item}>
+            <Link className={css.link} to={getAllIdeasRoute()}>
+              All Ideas
+            </Link>
+          </li>
+          <li className={css.item}>
             <Link className={css.link} to={getNewIdeaRoute()}>
               Add Idea
             </Link>
           </li>
-      </ul>
-    </div>
-    <div className={css.content}>
+          <li className={css.item}>
+            <Link className={css.link} to={getSignUpRoute()}>
+              Sign Up
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <div className={css.content}>
         <Outlet />
       </div>
     </div>
